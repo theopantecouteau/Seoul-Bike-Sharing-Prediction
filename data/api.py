fa74f3474867659cfb5ad8d0ede75f597efa83e6
Théo Pantecouteau <theo.pantecouteau@icloud.com>
2023-12-03 22:38:07 +0100

    web app ready, model ready

--- data/api.py
+++ data/api.py
@@ -2,26 +2,47 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import joblib
 from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 
 class PredictionRequest(BaseModel):
-    size: float
-    features: list
 
+    hour: int
+    temperature: float
+    humidity: float
+    windSpeed: float
+    visibility: float
+    solarRadiation: float
+    rainfall: float
+    snowfall: float
+    seasons: str
+    holiday: int
+    functioningDay: int
+    day: int
+    month: int
+    year: int
+    weekDay : str
 
 app = FastAPI()
-
+clean_df = pd.read_csv('./data/seoul-bike-data-clean-for-model.csv', encoding='unicode_escape').drop(columns=["Rented Bike Count"])
+column_names = clean_df.columns
 model = joblib.load('model.pkl')
 
 @app.post('/predict')
 async def predict(request: PredictionRequest):
     try:
-        features_array = request.features
-        predicted_number_of_rentals = model.predict([features_array])[0]
+        df = pd.DataFrame.from_dict(data=request.dict(), orient='index').T
+        df.columns = column_names
+        season_map = {'Winter': 1, 'Spring': 2, 'Summer': 3, 'Autumn': 4}
+        dict_day_of_week={'Monday':1,'Tuesday':2,'Wednesday':3,'Thursday':4,'Friday':5,'Saturday':6,'Sunday':7}
+        df['WeekDay'] = df['WeekDay'].map(dict_day_of_week)
+        df['Seasons'] = df['Seasons'].map(season_map)
+        print(df)
+        predicted_number_of_rentals = model.predict(df)
+        print("Predicted number of rentals: ", predicted_number_of_rentals)
         return {
-            'size': request.size,
-            'features': features_array,
-            'predicted_number_of_rentals': predicted_number_of_rentals
+            'prediction': predicted_number_of_rentals[0]
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
