f7ce991c9beead0c8791af85ef693553bf77af4f
Théo Pantecouteau <theo.pantecouteau@icloud.com>
2023-12-10 18:15:09 +0100

    final commit

--- data/api.py
+++ data/api.py
@@ -27,13 +27,20 @@ class PredictionRequest(BaseModel):
 app = FastAPI()
 clean_df = pd.read_csv('./data/seoul-bike-data-clean-for-model.csv', encoding='unicode_escape').drop(columns=["Rented Bike Count"])
 column_names = clean_df.columns
+columns_to_scale = ['Temperature(Â°C)', 'Humidity(%)', 'Wind speed (m/s)', 'Visibility (10m)', 'Solar Radiation (MJ/m2)',  'Rainfall(mm)', 'Snowfall (cm)']
 model = joblib.load('model.pkl')
+features_scaler = joblib.load('features_scaler.pkl')
+target_scaler = joblib.load('target_scaler.pkl')
 
 @app.post('/predict')
 async def predict(request: PredictionRequest):
     try:
         df = pd.DataFrame.from_dict(data=request.dict(), orient='index').T
         df.columns = column_names
+        print(df)
+        print(df.columns)
+        print(columns_to_scale)
+        df[columns_to_scale] = features_scaler.fit_transform(df[columns_to_scale])
         season_map = {'Winter': 1, 'Spring': 2, 'Summer': 3, 'Autumn': 4}
         dict_day_of_week={'Monday':1,'Tuesday':2,'Wednesday':3,'Thursday':4,'Friday':5,'Saturday':6,'Sunday':7}
         df['WeekDay'] = df['WeekDay'].map(dict_day_of_week)
@@ -41,8 +48,10 @@ async def predict(request: PredictionRequest):
         print(df)
         predicted_number_of_rentals = model.predict(df)
         print("Predicted number of rentals: ", predicted_number_of_rentals)
+        prediction = target_scaler.inverse_transform(predicted_number_of_rentals.reshape(-1, 1))
+        print("Predicted number of rentals: ", prediction)
         return {
-            'prediction': predicted_number_of_rentals[0]
+            'prediction': round(prediction[0][0])
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
